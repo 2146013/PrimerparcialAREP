2f9c582344704e10df9ff9dc8830e3d990ebd101
Daniela pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-09-09 09:22:49 -0500

    PARCIAL

---
+++ src/main/java/escuelaing/edu/AREP/App.java
@@ -0,0 +1,13 @@
+package escuelaing.edu.AREP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
